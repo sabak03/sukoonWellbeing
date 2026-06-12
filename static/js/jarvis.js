7265a3f05d56207a074b2f5444c0da903cb97d34
sabak03 <103966358+sabak03@users.noreply.github.com>
2026-06-12 18:47:04 +0100

    Update jarvis.js

--- static/js/jarvis.js
+++ static/js/jarvis.js
@@ -1,8 +1,17 @@
-function toggleMenu() {
-    const navLinks = document.getElementById("navLinks");
-    navLinks.classList.toggle("active");
-}
+const navLinks = document.getElementById("navLinks");
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navbar = document.querySelector(".navbar");
 
+  function toggleMenu() {
+    navLinks.classList.toggle("active");
+  }
+  document.addEventListener("click", function (event) {
+    const clickedInsideNavbar = navbar.contains(event.target);
+
+    if (!clickedInsideNavbar) {
+      navLinks.classList.remove("active");
+    }
+  });
 
 const revealElements = document.querySelectorAll(".reveal");
 
